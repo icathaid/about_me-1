1f4cd0f9452c3933f5ed5c834d216f4817d7f5fa
volodya12 <volodya12@hotmail.com>
2018-02-08 08:16:19 -0800

    More updated stuff in Question 7

--- js/game.js
+++ js/game.js
@@ -13,38 +13,62 @@
   }
 */
 
-var cityState = ['Puyallup', 'Tacoma', 'Federal\ Way', 'Russia', 'Washington'];
+var cityState = ['Puyallup', 'Tacoma', 'Federal\ Way', 'Russia', 'Washington', 'Oregon'];
 var totalGuess = 0;
 var flag = true;
-while(totalGuess < 6) && (flag === false)  {
-  var userImput = prompt('Guess what City and States I lived in').toLowerCase().trim();
-  for (var i = 0; i < cityState.length; i++) {
+  while(totalGuess < 6 && flag === false) {
+    var userInput = prompt('Guess what City and States I lived in').toLowerCase().trim();
+    for (var i = 0; i < cityState.length; i++) {
     var state = cityState[i];
-    if (state === userImput) {
+    if (state === userInput) {
       alert('good job');
-      flag = true
+      flag = true;
+      } else {
+      alert('oops, You\'re wrong');
+  } if (state[1] = userInput){
+      alert('good job');
+      flag = true;
+      } else {
+      alert('oops, You\'re wrong');
+  } if (state[2] = userInput) {
+      alert('good job');
+      } else {
+      alert('oops, You\'re wrong');
+  } if (state[3] = userInput){
+      alert('good job');
+      } else {
+      alert('oops, You\'re wrong');
+  } if (state[4] = userInput){
+      alert('good job');
     } else {
-      alert('oops, got it wrong');
-      if (state[1] = userImput){
-        alert('good job');
-      }else {
-        alert('oops, got it wrong');
-      } if ()
-
-      }
-      
+      alert('oops, You\'re wrong');
+  } if (state[5] = userInput){
+      alert('Good Job');
+    } else {
+      alert('oops, You\'re wrong');
+  } if (state[6] = userInput){
+      alert('Good job');
+    } else {
+      alert('oops, You\'re wrong');
+    }   
     }
-  }
-}
+    }
 
 
 
 
 
 
-for (var i = 0; i < cityState.length; i++){
-if (
-  userImput[i]  )
-  cityState
-  alert(cityState[x]
-  } if (
+             
+          
+        
+      
+    
+      
+      
+    
+
+          
+        
+      
+  
