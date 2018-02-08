1cc43a4f47ca5dfe4356f33059b1e81d22dc254b
James McDaniel <icathaid@gmail.com>
2018-02-08 14:41:42 -0800

    polished code, added functions

--- js/game.js
+++ js/game.js
@@ -1,19 +1,23 @@
 'use strict'
 
-
-/*for (var x = 0; x < 4; x++){
-  var myNumber = parseInt(prompt('Guess how much Gas tank my 2006 Honda Accord holds').toLowerCase().trim());
+function numberGame() {
+var guesses = 4;
+for (var x = 0; x < 4; x++){
+  var myNumber = parseInt(prompt('guess how much gas tank my honda accord holds').toLowerCase().trim());
   console.log('my number' + myNumber);
   if (myNumber < 15){
-    alert('Too low, try again');
+    guesses--;
+    alert('Too low, try again.  You have ' + guesses + ' left.' );
   } if (myNumber > 15){
-    alert ('Too high, try again');
+    guesses--;
+    alert ('Too high, try again ' + guesses + ' left.');
    } if (myNumber === 15){
       alert ('You got it');
+      x = 4;
   }
-*/
-
-
+}
+}
+function guessingGame() {
 var scoreCorrect = 0;
 var cityState = ['puyallup', 'tacoma', 'federal way'];
 var totalGuess = 6;
@@ -23,34 +27,21 @@ while(totalGuess > 0 && answerCorrect === false) {
   var userInput = prompt('Guess what City and States I lived in').toLowerCase().trim();
   for (var i = 0; i < cityState.length; i++){
     if (userInput === cityState[i]) {
-      alert('good job');
+      alert('good job.  I have lived in ' + cityState);
       scoreCorrect +=1;
       answerCorrect = true;
       break;
-    } 
+      } 
   } if (i >= cityState.length){
-    alert('oop, You\'re wrong ' + totalGuess);
+    alert('oops, You\'re wrong.  You have ' + totalGuess + ' guesses left.');
     console.log(totalGuess);
     totalGuess --;
-    
+  } if (totalGuess <1) {
+    alert('oops, you didn\'t get it.  The correct answers were ' + cityState);
   }
 }
+}
 
+numberGame();
+guessingGame();
 
-
-
-
-
-             
-          
-        
-      
-    
-      
-      
-    
-
-          
-        
-      
-  
